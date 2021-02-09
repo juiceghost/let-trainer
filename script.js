2729b5a04a3bb19dcecff7b261f5f1f8e37edafc
juiceghost <jg@renegade.se>
2021-02-09 09:17:29 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+for (var i = 0; i < 5; i++) {
+    var btn = document.createElement('button');
+    btn.appendChild(document.createTextNode('Button ' + i));
+    btn.addEventListener('click', function () {
+        console.log(i);
+    });
+    document.body.appendChild(btn);
+}
+
+var br = document.createElement('br');
+document.body.appendChild(br);
+
+for (let i = 0; i < 5; i++) {
+    var btn = document.createElement('button');
+    btn.appendChild(document.createTextNode('Button ' + i));
+    btn.addEventListener('click', function () {
+        console.log(i);
+    });
+    document.body.appendChild(btn);
+}
